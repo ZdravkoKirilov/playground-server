fede89ecd3bb4157e4864da9c2e39633c8ef8ecd
Zdravko Kirilov <Zdravko Kirilov@dxc.com>
2019-08-23 16:43:52 +0300

    initial setup

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectMongoose = async (url = 'mongodb://localhost:27017/playground') => {
-    await mongoose.connect(url);
+    await mongoose.connect(url, { useNewUrlParser: true });
     mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
     return Promise.resolve();
 };
